3b5c2375e9c985e6df1d9ef7348e6fa42c80754c
Ehsan Narmani <ehsan.enk.narmani@gmail.com>
2024-12-24 00:57:38 +0330

    inital commit

---
+++ worker.js
@@ -0,0 +1,37 @@
+const repositories = {
+    google: "https://dl.google.com/dl/android/maven2",
+    central: "https://repo.maven.apache.org/maven2",
+    jitpack: "https://jitpack.io",
+};
+export default {
+    async fetch(request) {
+        const url = new URL(request.url);
+
+        // Extract the first part of the path
+        const pathSegments = url.pathname.split('/').filter(Boolean); // Filter removes empty segments
+        const repoKey = pathSegments[0]; // First segment of the path
+
+        // Check if the URL starts with a specific repository key
+        if (repositories[repoKey]) {
+            const response= await fetchFromRepository(repositories[repoKey], url.pathname.replace(`/${repoKey}`, ''), request);
+            if(response.ok) return response
+        }else{
+            for (const repoUrl of Object.values(repositories)) {
+                const response = await fetchFromRepository(repoUrl, url.pathname, request);
+                if (response.ok) return response; // Return the response if successful
+            }
+        }
+
+        return new Response("Dependency Not Found", { status: 404 });
+    },
+};
+
+async function fetchFromRepository(baseUrl, path, request) {
+    const targetUrl = `${baseUrl}${path}`;
+
+    return await fetch(targetUrl, {
+        method: request.method,
+        headers: request.headers,
+        body: ['GET', 'HEAD'].includes(request.method) ? null : request.body,
+    });
+}
